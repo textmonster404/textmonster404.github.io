ff53e89379554e0edef4024eef4a183f238d6833
textmonster404 <31630195+textmonster404@users.noreply.github.com>
2018-05-30 15:24:03 -0700

    Made this...

---
+++ JoshCSS.js
@@ -0,0 +1,3 @@
+document.write("<link rel='stylesheet' href='https://textmonster404.github.io/h/JoshCSS.css'>");
+var triangles = document.getElementsByTagName("speech").getElementsByTagName("speech-triangle");
+for (var i=0;i<triangles.length;i++) {triangles[i].textContent=" ";}
