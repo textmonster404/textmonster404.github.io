d268ddbafd47c0716a87e13d2261208578610765
textmonster404 <31630195+textmonster404@users.noreply.github.com>
2018-05-30 16:53:11 -0700

    Updated This...

--- JoshCSS.js
+++ JoshCSS.js
@@ -1,3 +1,3 @@
 document.write("<link rel='stylesheet' href='https://textmonster404.github.io/h/JoshCSS.css'>");
-var triangles = document.getElementsByTagName("speech").getElementsByTagName("speech-triangle");
+var triangles = document.getElementsByTagName("speech-triangle");
 for (var i=0;i<triangles.length;i++) {triangles[i].textContent=" ";}
