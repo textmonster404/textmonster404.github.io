e57e5b9ceec01deadd5509cba5268825fbb934c2
textmonster404 <31630195+textmonster404@users.noreply.github.com>
2018-05-30 18:03:38 -0700

    updated stuff

--- JoshCSS.js
+++ JoshCSS.js
@@ -1,2 +1,2 @@
 document.write("<link rel='stylesheet' href='https://textmonster404.github.io/h/JoshCSS.css'>");
-window.onLoad = function() {document.body.innerHTML+="<script>var triangles = document.getElementsByTagName('speech-triangle');for (var i=0;i<triangles.length;i++) {triangles[i].innerHTML='Tri';}</script>";}
+document.onload = function() {document.body.innerHTML+="<script>var triangles = document.getElementsByTagName('speech-triangle');for (var i=0;i<triangles.length;i++) {triangles[i].innerHTML='Tri';}</script>";}
