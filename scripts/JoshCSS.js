1b673162099db8d7a9987558bf1336ac28202054
textmonster404 <31630195+textmonster404@users.noreply.github.com>
2018-10-29 01:43:10 -0700

    Update JoshCSS.js

--- scripts/JoshCSS.js
+++ scripts/JoshCSS.js
@@ -289,3 +289,8 @@ if (document.documentElement.innerHTML.includes("<use-tag ltxt")) {
         document.getElementsByTagName("ltxt")[i].innerHTML=ltxt(document.getElementsByTagName("ltxt")[i].innerHTML);
     }
 }
+if (document.querySelector("meta[http-equiv='reset-on-load']").getAttribute("content")=="true") {
+    window.onbeforeunload = function() {
+        window.scrollTo(0, 0);
+    }
+}
