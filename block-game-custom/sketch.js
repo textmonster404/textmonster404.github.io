960fbfafe959585c4dcae3e626d3c5a7735d846f
Josh Oliver <31630195+textmonster404@users.noreply.github.com>
2019-06-21 17:25:38 -0700

    Update sketch.js

--- block-game-custom/sketch.js
+++ block-game-custom/sketch.js
@@ -1,6 +1,6 @@
 function createGame(scr) {
 var ref = firebase.database().ref().push();
-console.log(firebase.database().ref(ref.getKey()).set(scr).then(function(d){var url = window.location.href.split("make").join("play")+"?g="+ref.getKey();document.body.innerHTML+="<div><a href=\""+url+"\">"+url+"</a></div>";}));
+console.log(firebase.database().ref(ref.getKey()).set(scr).then(function(d){var url = window.location.href.split("make").join("play")+"?g="+ref.getKey();createDiv("<a href=\""+url+"\">"+url+"</a>");}));
 }
 
 
