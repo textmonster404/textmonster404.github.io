499da7449635a4709e24afbdbccb153fad54404e
Josh Oliver <31630195+textmonster404@users.noreply.github.com>
2019-09-30 12:53:47 -0700

    Update checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -2,7 +2,7 @@ checkbox = {};
 checkbox.gameURLs = ["https://textmonster404.github.io/Random/random.html","https://textmonster404.github.io/","https://textmonster404.github.io/index.html","https://textmonster404.github.io/school/index.html","https://textmonster404.github.io/tools.html","https://textmonster404.github.io/Random/weird.html","https://textmonster404.github.io/bounce.html","https://textmonster404.github.io/html%20editor.html","https://textmonster404.github.io/maze/moveHole.html","https://textmonster404.github.io/maze/loop.html","https://textmonster404.github.io/maze/maze.html","https://textmonster404.github.io/maze/hole2.html","https://textmonster404.github.io/maze/trickHole.html","https://textmonster404.github.io/maze/door.html","https://textmonster404.github.io/maze/door2.html","https://textmonster404.github.io/maze/doorAgain.html","https://textmonster404.github.io/maze/doorAgain2.html","https://textmonster404.github.io/maze/doorBack.html","https://textmonster404.github.io/maze/doorBehind.html","https://textmonster404.github.io/maze/flip.html","https://textmonster404.github.io/maze/wallDoor.html","https://textmonster404.github.io/maze/wormHole.html"];
 checkbox.gameLinks = [];
 
-var uuidv4 = function(){return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));};
+var uuidv4 = (function(){return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));})();
 
 checkbox.url = localStorage.getItem("checkbox") || "";
 checkbox.wins = localStorage.getItem("checkbox-wins") || 0;
@@ -41,6 +41,7 @@ checkbox.end = function() {
     window.clearInterval(checkbox.interval);
     checkbox.url="";
     localStorage.setItem("checkbox","");
+    document.getElementById("checkbox-"+uuidv4).disabled = true;
 }
 checkbox.roulette = function() {
     var a = checkbox.gameLinks[Math.floor(Math.random()*checkbox.gameLinks.length)];
